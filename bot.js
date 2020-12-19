1e5a2f50b290ad04259c804698cdc2e87a67530b
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2020-12-18 17:09:18 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,36 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ws:{intents:["DIRECT_MESSAGES", "GUILDS"]}})
 const config = require("./config.json")
+let channels = require("./channels.json")
+const fs = require("fs")
+
+if(!fs.existsSync("./channels.json"))
+    fs.writeFileSync("./channels.json", JSON.stringify({}))
 
 client.on("ready", () => console.log("Ready"))
 client.on("message", (message) => {
     if(message.channel.type == "dm" && message.author.bot == false){
-        let channel = message.client.channels.cache.get(config.CHANNEL)
-        channel.send(`\`\`\`\n${message.content}\n\`\`\`\nMessage by <@${message.author.id}>`)
-        .then(msg => {
-            msg.pin()
-            .then(() => {
-                channel.messages.fetchPinned(true).then(messages => {
-                    message.channel.send(`📌 Pinned your message in <#${config.CHANNEL}> (${config.CHANNEL}). This was the ${messages.size} pinned message.`)  
+        if(!channel[message.author.id]){
+            message.guild.channels.create(message.author.id, {
+                topic:`This is ${message.author.tag}'s (${message.author.id}) channel.`,
+                type:"text",
+                parent:"786354821207949353",
+            }).then(channel => {
+                channels[message.author.id] = channel.id
+                fs.writeFileSync("./channels.json", JSON.stringify(channels))
+                channel.send(`\`\`\`\n${message.content}\n\`\`\`\nMessage by <@${message.author.id}>`)
+                .then(msg => {
+                    message.channel.send(`📤 Sent your message in <#${msg.channel.id}> (${msg.channel.id}). `)  
                 })
             })
-        })
+        }else{
+            let channel = client.channels.cache.get(channel[message.author.id])
+            channel.send(`\`\`\`\n${message.content}\n\`\`\`\nMessage by <@${message.author.id}>`)
+            .then(msg => {
+                message.channel.send(`📤 Sent your message in <#${msg.channel.id}> (${msg.channel.id}). `)  
+            })
+        }
     }
 })
-
-client.on("message", msg => console.log)
 
 client.login(config.BOT_TOKEN)
